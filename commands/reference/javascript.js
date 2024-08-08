7b3f8b825e67275436df63b779e02167127d3275
Thenlie <leithen113@gmail.com>
2024-08-07 18:54:40 -0600

    create section choice option

--- commands/reference/javascript.js
+++ commands/reference/javascript.js
@@ -1,6 +1,15 @@
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { jsTitles, getMDNFile } from 'jsman'
-import { getSection, getHeader } from 'jsman/dist/parser/index.js';
+import { getSection, getHeader, stripJsxRef } from 'jsman/dist/parser/index.js';
+
+const removeTitle = (doc) => {
+    // Find the index of the second newline character
+    const newlineIndex = doc.indexOf('\n', doc.indexOf('\n') + 1);
+    // If there are fewer than two newlines, return the original string
+    if (newlineIndex === -1) return doc;
+    // Return the substring starting after the second newline
+    return doc.substring(newlineIndex + 1);
+}
 
 const truncateString = (str) => {
     const MAX_LENGTH = 1024;
@@ -22,6 +31,25 @@ export default {
 	data: new SlashCommandBuilder()
 		.setName('javascript')
 		.setDescription('Search the MDN JavaScript documentation')
+        .addStringOption(option =>
+            option.setName('section')
+                .setDescription('Which section of the MDN document to return')
+                .setRequired(true)
+                .setChoices([
+                    {
+                        name: 'Description',
+                        value: 'Description'
+                    },
+                    {
+                        name: 'Parameters',
+                        value: 'Parameters'
+                    },
+                    {
+                        name: 'Examples',
+                        value: 'Examples'
+                    }
+                ])
+        )
 		.addStringOption(option => 
 			option.setName('query')
 				.setDescription('JavaScript reference to search for')
@@ -30,7 +58,6 @@ export default {
 		),
 	async autocomplete(interaction) {
 		const focusedValue = interaction.options.getFocused();
-		console.log(focusedValue)
 		let count = 0;
 		const filtered = choices.filter(choice => choice.name.includes(focusedValue));
 		await interaction.respond(
@@ -42,27 +69,27 @@ export default {
 		);
 	},
 	async execute(interaction) {
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		console.log(JSON.stringify(interaction.options._hoistedOptions[0].value))
-        const file = getMDNFile(interaction.options._hoistedOptions[0].value)
-        const params = getSection('Parameters', file);
+        const options = interaction.options._hoistedOptions;
+        const filepath = options.find(obj => obj.name === 'query').value
+        const section = options.find(obj => obj.name === 'section').value
+
+        if (!filepath || !section) {
+            console.error(`No filepath or section provided! Filepath: ${filepath}, Section: ${section}`)
+            await interaction.reply()
+            return;
+        }
+
+        const file = getMDNFile(filepath)
+        const document = getSection(section, file);
         const header = getHeader(file);
-        // let reply;
-        // if (header) {
-        //     reply = header.title + params
-        // } else {
-        //     reply = params
-        // }
-		// await interaction.reply(reply);
+
+        const strippedDoc = stripJsxRef(document);
  
         const exampleEmbed = new EmbedBuilder()
-            .setColor(0x0099FF)
+            .setColor(0x3170D6)
             .setTitle(header.title)
             .setURL(`https://developer.mozilla.org/en-US/docs/${header.slug}`)
-            .addFields(
-                { name: 'Parameters', value: truncateString(params) },
-            )
+            .setDescription(truncateString(strippedDoc))
         
             await interaction.reply({ embeds: [exampleEmbed] });
 	},
